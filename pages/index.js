3c7a7cda7424a7261e5d22960b59eea129084f26
imwithawank <inforschooldotcom@gmail.com>
2022-11-09 13:41:52 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <a href="https://www.w3schools.com">Visit W3Schools</a>
         </p>
       </main>
 
